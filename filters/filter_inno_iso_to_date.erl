aece2956e8b7a97d7d5779a06af18543e5513a59
Kirill Sysoev <kirill.sysoev@gmail.com>
2016-03-09 16:19:26 +0300

    update

--- filters/filter_inno_iso_to_date.erl
+++ filters/filter_inno_iso_to_date.erl
@@ -4,13 +4,17 @@
           inno_iso_to_date/3
 ]).
 
-inno_iso_to_date(ISOTime, _Context) ->
+inno_iso_to_date(ISOTime, _Context) when is_binary(ISOTime) ->
+    lager:info("ISOTime: ~p", [ISOTime]),
     <<Year:4/binary, _:1/binary, Month:2/binary, _:1/binary, Day:2/binary
      ,_:1/binary
      ,Hours:2/binary, _:1/binary, Minutes:2/binary, _:1/binary, Sec:2/binary, _/binary>> = ISOTime,
 
     {{z_convert:to_integer(Year),z_convert:to_integer(Month),z_convert:to_integer(Day)}
-    ,{z_convert:to_integer(Hours),z_convert:to_integer(Minutes),z_convert:to_integer(Sec)}}.
+    ,{z_convert:to_integer(Hours),z_convert:to_integer(Minutes),z_convert:to_integer(Sec)}};
+
+inno_iso_to_date(ISOTime, _Context) ->
+    ISOTime.
 
 inno_iso_to_date(Seconds, Args, _Context) ->
     lager:info("Args: ~p", [Args]),
