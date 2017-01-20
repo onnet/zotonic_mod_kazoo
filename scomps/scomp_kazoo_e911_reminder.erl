1ed51dce862ead578d7ba124adbd8f3a142bfe51
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-01-20 18:42:54 +0300

    update

--- scomps/scomp_kazoo_e911_reminder.erl
+++ scomps/scomp_kazoo_e911_reminder.erl
@@ -8,11 +8,17 @@
 vary(_Params, _Context) -> nocache.
 
 render(_Params, _Vars, Context) ->
-    Numbers = kazoo_util:kz_account_numbers_info(Context),
-    Features = lists:foldr(fun({_,Number}, Acc) -> modkazoo_util:get_value(<<"features">>, Number) ++ Acc end, [], Numbers),
-    case lists:member(<<"e911">>, Features) of 
-        'false' ->
-            {'ok', <<"<span class='reminder911'>Please configure E911 addresses!</span>">>};
-        'true' ->
-            {'ok', 'undefined'}
+    case kazoo_util:kz_account_numbers_info(Context) of
+        [] -> 
+            {'ok', 'undefined'};
+        Numbers ->
+            Features = lists:foldr(fun({_,Number}, Acc) -> modkazoo_util:get_value(<<"features">>, Number) ++ Acc end
+                                  ,[]
+                                  ,Numbers),
+            case lists:member(<<"e911">>, Features) of 
+                'false' ->
+                    {'ok', <<"<span class='reminder911'>Please configure E911 addresses!</span>">>};
+                'true' ->
+                    {'ok', 'undefined'}
+            end
     end.
