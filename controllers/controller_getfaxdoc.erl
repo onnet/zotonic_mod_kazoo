6941488327361d55c1c65b48c57b71b3bbe977d6
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-05-26 14:02:32 +0000

    update

--- controllers/controller_getfaxdoc.erl
+++ controllers/controller_getfaxdoc.erl
@@ -6,34 +6,30 @@
 
 -module(controller_getfaxdoc).
 -export([
-    init/1,
-    service_available/2,
-    allowed_methods/2,
-    resource_exists/2,
-    forbidden/2,
-    content_types_provided/2,
-    charsets_provided/2,
-    encodings_provided/2,
-    provide_content/2,
-    finish_request/2
+    service_available/1,
+    allowed_methods/1,
+    resource_exists/1,
+    forbidden/1,
+    content_types_provided/1,
+    charsets_provided/1,
+    encodings_provided/1,
+    provide_content/1,
+    finish_request/1
 ]).
 
 -include_lib("zotonic.hrl").
+-include("../include/mod_kazoo.hrl").
 
 -define(FILE_DIR, <<"/tmp/">>).
 
-init(ConfigProps) ->
-    {ok, ConfigProps}.
-
-%% @doc Initialize the context for the request. Continue session when available.
-service_available(ReqData, ConfigProps) ->
-    Context = z_context:set_noindex_header(z_context:set(ConfigProps, z_context:new(ReqData))),
-    Context1 = z_context:ensure_qs(z_context:continue_session(Context)),
-    DocId = z_context:get_q("id", Context1),
+service_available(Context0) ->
+    Context = z_context:set_noindex_header(Context0),
+    Context1 = z_context:continue_session(z_context:ensure_qs(Context)),
+    DocId = z_context:get_q('id', Context1),
     ContextMime = z_context:set(mime, z_media_identify:guess_mime(DocId), Context1),
-    case file:read_file(<<?FILE_DIR/binary, (z_convert:to_binary(DocId))/binary>>) of
+    case file:read_file(<<?FILE_DIR/binary, DocId/binary>>) of
         {ok, FileBody} ->
-            case byte_size(z_convert:to_binary(FileBody)) of
+            case byte_size(?TO_BIN(FileBody)) of
                 0 -> 
                     {false, Context1};
                 N when is_integer(N) ->
@@ -46,68 +42,72 @@ service_available(ReqData, ConfigProps) ->
             {false, Context1}
     end.
 
-allowed_methods(ReqData, Context) ->
-    {['HEAD', 'GET'], ReqData, Context}.
+allowed_methods(Context) ->
+    {[<<"HEAD">>, <<"GET">>], Context}.
 
-content_types_provided(ReqData, Context) ->
-    {[{z_context:get(mime, Context), provide_content}], ReqData, Context}.
-
-%% @doc Simple access control for rsc based files
-forbidden(ReqData, Context) ->
+forbidden(Context) ->
     case file_exists(Context) of
         'true' ->
-            {false, ReqData, Context};
+            {false, Context};
         'false' ->
-            {true, ReqData, Context}
+            {true, Context}
     end.
 
-encodings_provided(ReqData, Context) ->
+content_types_provided(Context) ->
+    {[{?TO_BIN(z_context:get(mime, Context)), provide_content}], Context}.
+
+encodings_provided(Context) ->
     Mime = z_context:get(mime, Context),
     Encodings = case z_media_identify:is_mime_compressed(Mime) of
-        true -> [{"identity", fun(Data) -> Data end}];
-        _    -> [{"identity", fun(Data) -> decode_data(identity, Data) end},
-                {"gzip",     fun(Data) -> decode_data(gzip, Data) end}]
+        true -> [{<<"identity">>, fun(Data) -> Data end}];
+        _    -> [{<<"identity">>, fun(Data) -> decode_data(identity, Data) end}
+                ,{<<"gzip">>, fun(Data) -> decode_data(gzip, Data) end}]
     end,
-    {Encodings, ReqData, z_context:set(encode_data, length(Encodings) > 1, Context)}.
+    {Encodings, z_context:set(encode_data, length(Encodings) > 1, Context)}.
 
-resource_exists(ReqData, Context) ->
-    {file_exists(Context), ReqData, Context}.
+resource_exists(Context) ->
+    {file_exists(Context), Context}.
 
-charsets_provided(ReqData, Context) ->
+charsets_provided(Context) ->
     case is_text(z_context:get(mime, Context)) of
-        true -> {[{"utf-8", fun(X) -> X end}], ReqData, Context};
-        _ -> {no_charset, ReqData, Context}
+        true -> {[{<<"utf-8">>, fun(X) -> X end}], Context};
+        _ -> {no_charset, Context}
     end.
-    
-provide_content(ReqData, Context) ->
-    Filename = z_context:get_q("id", Context),
-    RD1 = case z_context:get(content_disposition, Context) of
-              inline ->     wrq:set_resp_header("Content-Disposition", "inline; filename="++Filename, ReqData);
-              attachment -> wrq:set_resp_header("Content-Disposition", "attachment; filename="++Filename, ReqData);
-              undefined ->  ReqData
-          end,
+
+provide_content(Context0) ->
+    FileName = z_context:get_q('id', Context0),
+    Context = case z_context:get(content_disposition, Context0) of
+                  inline ->
+                      z_context:set_resp_header(<<"content-disposition">>
+                                               ,<<"inline; filename=", FileName/binary>>
+                                               ,Context0);
+                  attachment ->
+                      z_context:set_resp_header(<<"content-disposition">>
+                                               ,<<"attachment; filename=", FileName/binary>>
+                                               ,Context0);
+                  undefined ->  Context0
+              end,
     case z_context:get(body, Context) of
         undefined ->
-            {'ok', Data} = file:read_file(<<?FILE_DIR/binary, (z_convert:to_binary(z_context:get_q("id", Context)))/binary>>),
+            {'ok', Data} = file:read_file(<<?FILE_DIR/binary, FileName/binary>>),
             Body = case z_context:get(encode_data, Context, false) of 
                 true -> encode_data(Data);
                 false -> Data
             end,
-            {Body, RD1, z_context:set(body, Body, Context)};
+            {Body, z_context:set(body, Body, Context)};
         Body -> 
-            {Body, RD1, Context}
+            {Body, Context}
     end.
     
-finish_request(ReqData, Context) ->
-    {ok, ReqData, Context}.
+finish_request(Context) ->
+    {ok, Context}.
 
 %%%%%%%%%%%%%% Helper functions %%%%%%%%%%%%%%
 
 file_exists(Context) ->
-    DocId = z_context:get_q("id", Context),
-    case string:len(re:replace(z_convert:to_list(DocId), "[^A-Za-z0-9]", "", [global, {return, list}])) of
-        65 ->
-            case file:read_file_info(<<?FILE_DIR/binary, (z_convert:to_binary(DocId))/binary>>) of
+    case z_context:get_q('id', Context) of
+        <<"faxout-", _:32/binary, "-", _:24/binary, ".pdf">> = DocId ->
+            case file:read_file_info(<<?FILE_DIR/binary, DocId/binary>>) of
                  {'ok',_} -> 'true';
                  _ -> 'false'
             end;
--- controllers/controller_getinfaxdoc.erl
+++ controllers/controller_getinfaxdoc.erl
@@ -4,30 +4,26 @@
 
 -module(controller_getinfaxdoc).
 -export([
-    init/1,
-    service_available/2,
-    allowed_methods/2,
-    resource_exists/2,
-    forbidden/2,
-    content_types_provided/2,
-    charsets_provided/2,
-    encodings_provided/2,
-    provide_content/2,
-    finish_request/2
+    service_available/1,
+    allowed_methods/1,
+    resource_exists/1,
+    forbidden/1,
+    content_types_provided/1,
+    charsets_provided/1,
+    encodings_provided/1,
+    provide_content/1,
+    finish_request/1
 ]).
 
 -include_lib("zotonic.hrl").
+-include("../include/mod_kazoo.hrl").
 
-init(ConfigProps) ->
-    {ok, ConfigProps}.
-
-%% @doc Initialize the context for the request. Continue session when available.
-service_available(ReqData, ConfigProps) ->
-    Context = z_context:set_noindex_header(z_context:set(ConfigProps, z_context:new(ReqData))),
-    Context1 = z_context:ensure_qs(z_context:continue_session(Context)),
-    DocId = z_context:get_q("id", Context1),
+service_available(Context0) ->
+    Context = z_context:set_noindex_header(Context0),
+    Context1 = z_context:continue_session(z_context:ensure_qs(Context)),
+    DocId = z_context:get_q('id', Context1),
     ContextMime = z_context:set(mime, "application/pdf", Context1),
-    case byte_size(z_convert:to_binary(kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context1))) of
+    case byte_size(?TO_BIN(kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context1))) of
         0 -> {false, Context1};
         N when is_integer(N) ->
             ContextSize = z_context:set([{file_size, N}], ContextMime), 
@@ -36,69 +32,73 @@ service_available(ReqData, ConfigProps) ->
             {false, Context1}
     end.
 
-allowed_methods(ReqData, Context) ->
-    {['HEAD', 'GET'], ReqData, Context}.
+allowed_methods(Context) ->
+    {[<<"HEAD">>, <<"GET">>], Context}.
 
-content_types_provided(ReqData, Context) ->
-    {[{z_context:get(mime, Context), provide_content}], ReqData, Context}.
-
-%% @doc Simple access control for rsc based files
-forbidden(ReqData, Context) ->
-    DocId = z_context:get_q("id", Context),
-    case string:len(re:replace(z_convert:to_list(DocId), "[^A-Za-z0-9]", "", [global, {return, list}])) of
-        38 ->
+forbidden(Context) ->
+    case z_context:get_q('id', Context) of
+        <<_:6/binary, "-", _:32/binary, ".pdf">> = DocId ->
             case kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context) of
                  <<"">> ->
-                     {true, ReqData, Context};
+                     {true, Context};
                  _ ->
-                     {false, ReqData, Context}
+                     {false, Context}
             end;
         _ ->
-            {true, ReqData, Context}
+            {true, Context}
     end.
 
+content_types_provided(Context) ->
+    {[{?TO_BIN(z_context:get(mime, Context)), provide_content}], Context}.
 
-encodings_provided(ReqData, Context) ->
+encodings_provided(Context) ->
     Mime = z_context:get(mime, Context),
     Encodings = case z_media_identify:is_mime_compressed(Mime) of
-        true -> [{"identity", fun(Data) -> Data end}];
-        _    -> [{"identity", fun(Data) -> decode_data(identity, Data) end},
-                {"gzip",     fun(Data) -> decode_data(gzip, Data) end}]
+        true -> [{<<"identity">>, fun(Data) -> Data end}];
+        _    -> [{<<"identity">>, fun(Data) -> decode_data(identity, Data) end}
+                ,{<<"gzip">>, fun(Data) -> decode_data(gzip, Data) end}]
     end,
-    {Encodings, ReqData, z_context:set(encode_data, length(Encodings) > 1, Context)}.
+    {Encodings, z_context:set(encode_data, length(Encodings) > 1, Context)}.
 
-resource_exists(ReqData, Context) ->
-    DocId = z_context:get_q("id", Context),
-    {kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context) =/= <<>>, ReqData, Context}.
+resource_exists(Context) ->
+    DocId = z_context:get_q('id', Context),
+    {kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context) =/= <<>>, Context}.
 
-charsets_provided(ReqData, Context) ->
+charsets_provided(Context) ->
     case is_text(z_context:get(mime, Context)) of
-        true -> {[{"utf-8", fun(X) -> X end}], ReqData, Context};
-        _ -> {no_charset, ReqData, Context}
+        true -> {[{<<"utf-8">>, fun(X) -> X end}], Context};
+        _ -> {no_charset, Context}
     end.
     
-provide_content(ReqData, Context) ->
-    DocId = z_context:get_q("id", Context),
-    Filename = DocId ++ ".pdf",
-    RD1 = case z_context:get(content_disposition, Context) of
-              inline ->     wrq:set_resp_header("Content-Disposition", "inline; filename="++Filename, ReqData);
-              attachment -> wrq:set_resp_header("Content-Disposition", "attachment; filename="++Filename, ReqData);
-              undefined ->  ReqData
-          end,
+provide_content(Context0) ->
+    DocId = z_context:get_q('id', Context0),
+    FileName = <<DocId/binary, ".pdf">>,
+    Context = case z_context:get(content_disposition, Context0) of
+                  inline ->
+                      z_context:set_resp_header(<<"content-disposition">>
+                                               ,<<"inline; filename=", FileName/binary>>
+                                               ,Context0);
+                  attachment ->
+                      z_context:set_resp_header(<<"content-disposition">>
+                                               ,<<"attachment; filename=", FileName/binary>>
+                                               ,Context0);
+                  undefined ->
+                      Context0
+              end,
     case z_context:get(body, Context) of
         undefined ->
-            Data = z_convert:to_binary(kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context)),
+            Data = ?TO_BIN(kazoo_util:kz_incoming_fax_attachment_pdf(DocId, Context)),
             Body = case z_context:get(encode_data, Context, false) of 
                 true -> encode_data(Data);
                 false -> Data
             end,
-            {Body, RD1, z_context:set(body, Body, Context)};
+            {Body, z_context:set(body, Body, Context)};
         Body -> 
-            {Body, RD1, Context}
+            {Body, Context}
     end.
     
-finish_request(ReqData, Context) ->
-    {ok, ReqData, Context}.
+finish_request(Context) ->
+    {ok, Context}.
 
 %%%%%%%%%%%%%% Helper functions %%%%%%%%%%%%%%
 
