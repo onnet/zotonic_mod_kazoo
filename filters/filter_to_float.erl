5bf4eff9e30297331c6a3a2d9cfc81c95b500811
Kirill Sysoev <kirill.sysoev@gmail.com>
2017-08-25 14:49:28 +0000

    update

--- filters/filter_to_float.erl
+++ filters/filter_to_float.erl
@@ -4,14 +4,12 @@
 to_float(undefined, _Context) ->
     undefined;
 to_float([N|_], _Context) ->
-lager:info("N: ~p",[N]),
 	try
 	    z_convert:to_float(N)
 	catch
 		_:_ -> undefined
 	end;
 to_float(N, _Context) ->
-lager:info("N: ~p",[N]),
 	try
 	    z_convert:to_float(N)
 	catch
